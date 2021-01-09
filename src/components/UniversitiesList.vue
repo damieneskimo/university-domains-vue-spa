17a7477790d740b42ae82b17bc05d990acfd9904
Damien Jiang <damieneskimo@gmail.com>
2021-01-10 03:26:50 +1300

    add search by name or domain

--- src/components/UniversitiesList.vue
+++ src/components/UniversitiesList.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
-    <input v-model="keywords" 
+    <input v-model="keywords"
       placeholder="Search by name or domain"
       class="border rounded w-1/3 px-3 py-1 float-right mb-5">
 
-    <table class="mt-14 mx-auto clear-both">
+    <table class="mt-14 mx-auto clear-both w-full">
       <thead>
         <tr>
-          <th class="text-left px-10">Name</th>
-          <th class="text-left">Domains</th>
+          <th class="text-left px-10 w-2/3">Name</th>
+          <th class="text-left w-1/3">Domains</th>
         </tr>
       </thead>
       <tbody class="text-left">
         <University 
-          v-for="university in universities"
+          v-for="university in filteredUniversities"
           :key="university.id"
           :name="university.name"
           :domains="university.domains" />
@@ -39,6 +39,14 @@
         keywords: ''
       }
     },
+    computed: {
+      filteredUniversities() {
+        return this.universities.filter(university => {
+          let haystack = (university.name + ' ' + university.domains.join(' ')).toLowerCase();
+          return haystack.includes(this.keywords.toLowerCase())
+        })
+      }
+    }
   }
 </script>
 
